927a1079d6faec798705ed9a2c4cebf57865d15b
POP CODE <lolo4ka69@list.ru>
2023-04-28 13:55:00 +0300

    Test commit2

--- src/test/java/ru/netology/CardDeliveryPattern/DeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryPattern/DeliveryTest.java
@@ -8,8 +8,7 @@ import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -38,16 +37,17 @@ class DeliveryTest {
         $(byText("Запланировать")).click();
         $(byText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
         $("[data-test-id='success-notification'] .notification__content")
-                .shouldHave(exactText("Встреча успешно запланирована на" + firstMeetingDate))
+                .shouldHave(exactText("Встреча успешно запланирована на " + firstMeetingDate))
                 .shouldBe(visible);
         $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT,Keys. HOME), Keys.BACK_SPACE);
         $("[data-test-id=date] input").setValue(secondMeetingDate);
         $(byText("Запланировать")).click();
         $("[data-test-id='replan-notification'] .notification__content")
-                .shouldHave(exactText("У Вас уже запланирована встреча на другую дату. Перепланировать?"))
+                .shouldHave(text("У Вас уже запланирована встреча на другую дату. Перепланировать? "))
                 .shouldBe(visible);
-        $("[data-test-id='replan-notification'] .notification__content")
-                .shouldHave(exactText("Встреча успешно запланирована на" + secondMeetingDate))
+        $("[data-test-id='replan-notification'] button").click();
+        $("[data-test-id='success-notification'] .notification__content")
+                .shouldHave(exactText("Встреча успешно запланирована на " + secondMeetingDate))
                 .shouldBe(visible);
 
     }
